37d75b597894420848c2a13b5d91cc34132f00c3
bayley <bayley019@outlook.com>
2025-09-23 16:36:26 -0400

    Initial Commit

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by bayley on 9/18/25.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("ConversionViewController loaded its view.")
+    }
+
+
+}
+
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by bayley on 9/23/25.
+//
+
+//import Foundation
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
+
