a78ab8f166794c059fc3f0f4d5ae63fd6cc5ab76
bayley <bayley019@outlook.com>
2025-09-24 13:44:42 -0400

    Acticate Constraints

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,8 +7,43 @@
 
 //import Foundation
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController{
+    var mapView: MKMapView!
+    
+    override func loadView(){
+        // Create a map view
+        mapView = MKMapView()
+        
+        // Set it as *the* view of this view controller
+        view = mapView
+        
+        // Segmented Controls
+        let segmentedControl
+                    = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor =  UIColor.systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        
+        // Define Constraints
+        let topConstraint =
+            segmentedControl.topAnchor.constraint(equalTo: view.topAnchor)
+        let leadingConstraint =
+            segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        let trailingConstraint =
+            segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+     
+        // Activate Contraints
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+    }
+
+
     
     override func viewDidLoad(){
         super.viewDidLoad()
